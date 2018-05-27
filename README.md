cb1d7e2460147955877e4b8d1f4e11f8c331020a
李旭 <461229187@qq.com>
2018-05-27 23:44:56 +0800

    `v1.1.10` `18.05.27` : 优化输入方式,目前可以填写项目名\版本号\端口号选项

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 <p align="center"><img width="160" src="https://github.com/codexu/x-build/blob/x-build4.1/src/assets/images/logo.png?raw=true" alt="x-build"></p>
 
 <p align="center">
-  <img src="https://img.shields.io/badge/version-1.1.9-blue.svg">
+  <img src="https://img.shields.io/badge/version-1.1.10-blue.svg">
 </p>
 
 <h1 align="center">x-build-cli</h1>
 
-命令行工具：`x-build-cli`，将此项安装到本地全局环境下，可以通过指令`x-build init [项目名]`快速初始化一个最新版本的`x-build`项目。
+命令行工具：`x-build-cli`，将此项安装到本地全局环境下，可以通过指令`x-build init`快速初始化一个最新版本的`x-build`项目。
 
 ## 起步
 
@@ -16,8 +16,9 @@
 ```
 
 ```
-  /* 初始化x-build项目 */
-  x-build init [项目名] or x init [项目名]
+  /* 初始化x-build项目 (简写为: x i) */
+  x-build init
+  // 输入项目名\版本号\端口号
 ```
 
 ## 参考文档
@@ -25,6 +26,8 @@
 x-build文档: [https://github.com/codexu/x-build](https://github.com/codexu/x-build)
 
 ## update
+
+`v1.1.10` `18.05.27` : 优化输入方式,目前可以填写项目名\版本号\端口号选项
 
 `v1.1.9` `18.05.22` : x-build4.3.0切换到master，以后从master拉区最新的x-build。
 
--- bin/x-build.js
+++ bin/x-build.js
@@ -1,51 +1,70 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 const path = require('path');
 const fs = require('fs');
 
 const program = require('commander');
+const co = require('co')
+const prompt = require('co-prompt')
 const download = require('download-git-repo');
 const chalk = require('chalk');
 const ora = require('ora');
 
 program
   .version('0.1.0')
-  .option('-i, init [name]', '初始化x-build项目')
+  .option('i, init', '初始化x-build项目')
 
 program
   .parse(process.argv);
 
 if (program.init) {
-  const spinner = ora('正在从github下载x-build').start();
-  download('codexu/x-build', program.init, function (err) {
-    if(!err){
-      spinner.clear()
-      console.info('');
-      console.info(chalk.green('-----------------------------------------------------'));
-      console.info('');
-      spinner.succeed(['项目创建成功,请继续进行以下操作:'])
-      console.info('');
-      console.info(chalk.blueBright(` -  cd ${program.init}`));
-      console.info(chalk.blueBright(` -  npm install`));
-      console.info(chalk.blueBright(` -  npm run dev`));
-      console.info('');
-      console.info(chalk.gray('参考文档: https://github.com/codexu/x-build'));
-      console.info('');
-      console.info(chalk.green('-----------------------------------------------------'));
-      console.info('');
-      console.log();
-      
-      fs.readFile(`${process.cwd()}/${program.init}/package.json`, (err, data) => {
-        if (err) throw err;
-        let _data = JSON.parse(data.toString())
-        _data.name = program.init
-        _data.version = '1.0.0'
-        let str = JSON.stringify(_data, null, 4);
-        fs.writeFile(`${process.cwd()}/${program.init}/package.json`, str, function (err) {
+
+  co(function* () {
+    console.info('');
+    // 分步接收用户输入的参数
+    let tplName = yield prompt(` - 请输入项目名${chalk.gray(`('x-build')`)}: `);
+    let tplVersion = yield prompt(` - 请输入版本号${chalk.gray(`('0.0.1')`)}: `);
+    let tplPort = yield prompt(` - 请输入端口号${chalk.gray(`('3000')`)}: `);
+    tplName = tplName ? tplName : 'x-build';
+    tplVersion = tplVersion ? tplVersion : '0.0.1';
+    tplPort = tplPort ? tplPort : '3000';
+    console.info('');
+    const spinner = ora('正在从github下载x-build').start();
+    download('codexu/x-build', tplName, function (err) {
+      if (!err) {
+        spinner.clear()
+        console.info('');
+        console.info(chalk.green('-----------------------------------------------------'));
+        console.info('');
+        spinner.succeed(['项目创建成功,请继续进行以下操作:'])
+        console.info('');
+        console.info(chalk.blueBright(` -  cd ${tplName}`));
+        console.info(chalk.blueBright(` -  npm install`));
+        console.info(chalk.blueBright(` -  npm run dev`));
+        console.info('');
+        console.info(chalk.gray(`devServer: http://localhost:${tplPort}`));
+        console.info('');
+        console.info(chalk.gray('参考文档: https://github.com/codexu/x-build'));
+        console.info('');
+        console.info(chalk.green('-----------------------------------------------------'));
+        console.info('');
+
+        fs.readFile(`${process.cwd()}/${tplName}/package.json`, (err, data) => {
           if (err) throw err;
-        })
-      });
-    }else{
-      spinner.warn(['发生错误，请在https://github.com/codexu，Issues留言'])
-    }
+          let _data = JSON.parse(data.toString())
+          _data.name = tplName
+          _data.version = tplVersion
+          _data.port = tplPort
+          let str = JSON.stringify(_data, null, 4);
+          fs.writeFile(`${process.cwd()}/${tplName}/package.json`, str, function (err) {
+            if (err) throw err;
+            process.exit()
+          })
+        });
+      } else {
+        spinner.warn(['发生错误，请在https://github.com/codexu，Issues留言'])
+        process.exit()
+      }
+    })
+
   })
 }
