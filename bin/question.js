9825f8b7268d97fa7393dbe3ed0a53ac667946c9
codexu <461229187@qq.com>
2018-10-23 16:06:26 +0800

    build: question add package_manager plugin

--- bin/question.js
+++ bin/question.js
@@ -23,6 +23,35 @@ const question = {
     message: `是否使用px2rem布局?`,
     name: 'rem',
     default: true
+  },
+  package_manager: {
+    type: 'list',
+    message: `包管理器: `,
+    name: 'package_manager',
+    choices: ['yarn', 'npm', 'cnpm']
+  },
+  plugin: {
+    type: 'checkbox',
+    message: `插件: `,
+    name: 'plugin',
+    choices: [
+      {
+        name: 'normalize.css',
+        checked: true
+      },
+      {
+        name: 'x-load',
+        checked: false
+      },
+      {
+        name: 'x-animate',
+        checked: false
+      },
+      {
+        name: 'animate.css',
+        checked: false
+      }
+    ]
   }
 }
 
--- bin/x-build.js
+++ bin/x-build.js
@@ -6,8 +6,9 @@ const fs = require('fs');
 const commander = require('commander');
 const inquirer = require('inquirer')
 const download = require('download-git-repo');
-// const chalk = require('chalk');
+const chalk = require('chalk');
 const ora = require('ora');
+const cmd = require('node-cmd');
 
 // const deleteFolder = require('./deleteFolder.js');
 const package = require('../package.json');
@@ -27,20 +28,15 @@ if (commander.init) {
     question.name,
     question.version,
     question.port,
-    question.rem
+    question.rem,
+    question.package_manager,
+    question.plugin
   ]).then(function (answers) {
     hint.line()
-    const spinner = ora('Downloading from GitHub').start();
+    const spinner = ora('正在下载最新模板...').start();
     download('codexu/x-build', answers.name, function (err) {
       if (!err) {
-        spinner.clear()
-        spinner.succeed(['项目初始化完成，请继续进行以下操作:'])
-        hint.print('cyan', ` $ cd ${answers.name}`, 'top')
-        hint.print('cyan', ` $ yarn / npm install`)
-        hint.print('cyan', ` $ npm run dev`, 'bottom')
-        hint.print('greenBright', `Server: http://localhost:${answers.port}`)
-        hint.print('greenBright', `Docs: https://codexu.github.io/`)
-        hint.line()
+        // 修改package.json
         fs.readFile(`${process.cwd()}/${answers.name}/package.json`, (err, data) => { 
           if (err) throw err;
           let _data = JSON.parse(data.toString())
@@ -51,9 +47,68 @@ if (commander.init) {
           let str = JSON.stringify(_data, null, 4);
           fs.writeFile(`${process.cwd()}/${answers.name}/package.json`, str, function (err) {
             if (err) throw err;
-            process.exit()
           })
         });
+        let package_manager = null;
+        switch (answers.package_manager) {
+          case 'npm':
+            package_manager = 'npm install'
+            break;
+          case 'cnpm':
+            package_manager = 'cnpm install'
+            break;
+          default:
+            package_manager = 'yarn'
+            break;
+        }
+        spinner.succeed(['模板下载完成.'])
+        spinner.clear()
+        spinner.start([`正在使用${chalk.greenBright(answers.package_manager)}安装项目依赖...`])
+        cmd.get(
+          `
+           cd ${answers.name}
+           ${package_manager}
+          `, 
+          function(err, data, stderr){
+            spinner.succeed(['项目依赖安装完成.'])
+            spinner.clear()
+            spinner.start([`正在使用${chalk.greenBright(answers.package_manager)}安装插件...`])
+            if(answers.rem === true){
+              answers.plugin.push('hotcss')
+            }
+            let plugin = answers.plugin.join(' ')
+            switch (answers.package_manager) {
+              case 'npm':
+                package_manager = `npm install ${plugin} --save`
+                break;
+              case 'cnpm':
+                package_manager = `cnpm install ${plugin} --save`
+                break;
+              default:
+                package_manager = `yarn add ${plugin}`
+                break;
+            }
+            cmd.get(
+              `
+                cd ${answers.name}
+                ${package_manager}
+              `, 
+              function() {
+                spinner.succeed([`插件安装完成.`])
+                spinner.clear()
+                hint.line()
+                setTimeout(function(){
+                  hint.print('green', ` 欢迎使用x-build,请继续完成以下操作:`, 'bottom')
+                  hint.print('cyan', ` $ cd ${answers.name}`)
+                  hint.print('cyan', ` $ npm run dev`, 'bottom')
+                  hint.print('green', ` [开发文档] https://codexu.github.io/`)
+                  hint.line()
+                  process.exit()
+                  }, 500)
+              }
+            )
+          }
+        );
       } else {
         spinner.warn(['warning！ 请在 https://github.com/codexu/x-build 留言'])
         process.exit()
